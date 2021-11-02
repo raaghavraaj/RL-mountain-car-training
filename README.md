67f783bb841a7e4c7693fbfffaa29acba72067dd
Raaghav Raaj <raaghavraaj@Raaghavs-MacBook-Pro.local>
2021-11-02 20:24:52 +0530

    final commit, report added

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ The repository contains the code to train the mountain car using SARSA method.
 Please head over to the [assignment webpage](https://www.cse.iitb.ac.in/~shivaram/teaching/cs747-a2021/pa-3/programming-assignment-3.html) on the instructor's website.
 
 The state of the car is described using two variables `(x, v)` where `x` is the position of the car in the one-dimensional track and `v` is the velocity.
+
+The script `mountain_car.py` has the following two arguments:
+1. `--task`: the options are `T1/T2`
+2. `--train`: the options are 0/1 where 1 is for training the weights, 0 is for using the trained weights
+
+Please use a command similar to the following in the `submission` directory to run the script:
+```
+RL-mountain-car-training/submission $ python3 mountain_car.py --task T1 --train 1
+```
--- submission/mountain_car.py
+++ submission/mountain_car.py
@@ -15,6 +15,8 @@ import time
 import argparse
 import matplotlib.pyplot as plt
 
+np.random.seed(42)
+
 
 class sarsaAgent():
     '''
@@ -29,17 +31,25 @@ class sarsaAgent():
 
     def __init__(self):
         self.env = gym.make('MountainCar-v0')
-        self.epsilon_T1 = None
-        self.epsilon_T2 = None
-        self.learning_rate_T1 = None
-        self.learning_rate_T2 = None
-        self.weights_T1 = None
-        self.weights_T2 = None
+        self.ranges_x1 = 50
+        self.ranges_v1 = 100
+        self.ranges_x2 = 30
+        self.ranges_v2 = 40
+        self.tiles = 7
+        self.epsilon_T1 = 0.1
+        self.epsilon_T2 = 0.1
+        self.learning_rate_T1 = 0.3
+        self.learning_rate_T2 = 0.5
+        self.weights_T1 = np.zeros((self.ranges_x1 + 1, self.ranges_v1 + 1, 3))
+        self.weights_T2 = np.zeros(
+            (self.tiles, self.ranges_x2 + 1, self.ranges_v2 + 1, 3))
         self.discount = 1.0
         self.train_num_episodes = 10000
         self.test_num_episodes = 100
-        self.upper_bounds = [self.env.observation_space.high[0], self.env.observation_space.high[1]]
-        self.lower_bounds = [self.env.observation_space.low[0], self.env.observation_space.low[1]]
+        self.upper_bounds = [
+            self.env.observation_space.high[0], self.env.observation_space.high[1]]
+        self.lower_bounds = [
+            self.env.observation_space.low[0], self.env.observation_space.low[1]]
 
     '''
     - get_table_features: Graded
@@ -48,16 +58,38 @@ class sarsaAgent():
     '''
 
     def get_table_features(self, obs):
-        return None
+        x, v = obs[0], obs[1]
+        N, M = self.ranges_x1, self.ranges_v1
+        state = np.zeros((N+1, M+1))
+        a = 1.8/N
+        b = 0.14/M
+        i = int((x+1.2)/a)
+        j = int((v+0.07)/b)
+        state[i][j] = 1
+        return state
 
     '''
     - get_better_features: Graded
     - Use this function to solve the Task-2
     - It should return representation of state.
+    - Use Tile Coding to complete this function.
     '''
 
     def get_better_features(self, obs):
-        return None
+        x, v = obs[0], obs[1]
+        N, M = self.ranges_x2, self.ranges_v2
+        state = np.zeros((self.tiles, N+1, M+1))
+        a = 1.8/N
+        b = 0.14/M
+        for ti in range(self.tiles):
+            i = int((x+1.2)/a)
+            i = i % (N+1)
+            j = int((v+0.07)/b)
+            j = j % (M+1)
+            x += 1/self.tiles
+            v += 1/self.tiles
+            state[ti, i, j] = 1
+        return state
 
     '''
     - choose_action: Graded.
@@ -67,7 +99,17 @@ class sarsaAgent():
     '''
 
     def choose_action(self, state, weights, epsilon):
-        return None
+        if np.random.uniform() < epsilon:
+            return np.random.randint(3)
+        else:
+            X = np.zeros(3)
+            if weights.shape[0] == self.tiles:
+                for i in range(3):
+                    X[i] = np.sum(state * weights[:, :, :, i])
+            else:
+                for i in range(3):
+                    X[i] = np.sum(state * weights[:, :, i])
+            return np.argmax(X)
 
     '''
     - sarsa_update: Graded.
@@ -78,7 +120,21 @@ class sarsaAgent():
     '''
 
     def sarsa_update(self, state, action, reward, new_state, new_action, learning_rate, weights):
-        return None
+        if weights.shape[0] == self.tiles:
+            for i in range(self.tiles):
+                target = reward + self.discount * \
+                    np.sum(new_state[i, :, :] * weights[i, :, :, new_action])
+                target -= np.sum(state[i, :, :] * weights[i, :, :, action])
+                target = learning_rate * target
+                weights[i, :, :, action] += target * state[i, :, :]
+            return weights
+        else:
+            target = reward + self.discount * \
+                np.sum(new_state * weights[:, :, new_action])
+            target -= np.sum(state * weights[:, :, action])
+            target = learning_rate * target
+            weights[:, :, action] += state*target
+            return weights
 
     '''
     - train: Ungraded.
@@ -100,11 +156,13 @@ class sarsaAgent():
         reward_list = []
         plt.clf()
         plt.cla()
+        # print("hi")
         for e in range(self.train_num_episodes):
             current_state = get_features(self.env.reset())
             done = False
             t = 0
             new_action = self.choose_action(current_state, weights, epsilon)
+            # print(e)
             while not done:
                 action = new_action
                 obs, reward, done, _ = self.env.step(action)
@@ -117,8 +175,10 @@ class sarsaAgent():
                     reward_list.append(-t)
                     break
                 t += 1
+        # print("hi")
         self.save_data(task)
-        reward_list=[np.mean(reward_list[i-100:i]) for i in range(100,len(reward_list))]
+        reward_list = [np.mean(reward_list[i-100:i])
+                       for i in range(100, len(reward_list))]
         plt.plot(reward_list)
         plt.savefig(task + '.jpg')
 
@@ -176,12 +236,12 @@ class sarsaAgent():
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
     ap.add_argument("--task", required=True,
-       help="first operand", choices={"T1", "T2"})
+                    help="first operand", choices={"T1", "T2"})
     ap.add_argument("--train", required=True,
-       help="second operand", choices={"0", "1"})
+                    help="second operand", choices={"0", "1"})
     args = vars(ap.parse_args())
-    task=args['task']
-    train=int(args['train'])
+    task = args['task']
+    train = int(args['train'])
     agent = sarsaAgent()
     agent.env.seed(0)
     np.random.seed(0)
